eb230584e3f9a473838a43c2c9bd01bda992360e
Tianle Liu <liutianle@cs.uchicago.edu>
2021-05-07 20:07:07 -0500

    add action seletor

--- scripts/execute_path.py
+++ scripts/execute_path.py
@@ -15,6 +15,17 @@ class ExecutePath(object):
         rospy.init_node("execute_path")
         rospy.sleep(1)
 
+        # Fetch actions. These are the only 9 possible actions the system can take.
+        # self.actions is an array of dictionaries where the row index corresponds
+        # to the action number, and the value has the following form:
+        # { dumbbell: "red", block: 1}
+        colors = ["red", "green", "blue"]
+        self.actions = np.loadtxt(path_prefix + "actions.txt")
+        self.actions = list(map(
+            lambda x: {"dumbbell": colors[int(x[0])], "block": int(x[1])},
+            self.actions
+        ))
+
         # Set up action publisher
         self.action_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveDBToBlock, queue_size=10)
 
@@ -24,20 +35,35 @@ class ExecutePath(object):
         # Initialize Q matrix
         self.qmatrix = []
 
+        # Initialize state to 0
+        self.state = 0
+
     def read_qmatrix(self):
         with open('qmatrix.csv', newline='') as csvfile:
             csvreader = csv.reader(csvfile, delimiter=',')
             for state in csvreader:
                 self.qmatrix.append([int(action) for action in state])
-
-        print(self.qmatrix)
         return
 
+    def move(self):
+        """Find the move with largest reward; if there are multiple, choose the 
+        first one. Then publish the action"""
+        
+        max_reward = max(self.qmatrix[self.state])
+        best_action = self.qmatrix[self.state].index(max_reward)
+        action = self.actions[best_action]
 
-    def find_next_best_move(self):
-        # TODO
+        # Publish the action
+        self.action.robot_db = action.get("dumbbell")
+        self.action.block_id = action.get("block")
+        self.action_pub.publish(self.action)
         return
+
+    def run(self):
+        self.read_qmatrix()
+        
+        if self.qmatrix:
+            self.move()
 
 if __name__ == "__main__":
     node = ExecutePath()
-    node.read_qmatrix()
